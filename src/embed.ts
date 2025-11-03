c49582851da7cfe76fc71da8d06bf2fcfb62b14e
Andreas Rozek <A.Rozek@gmx.de>
2025-11-03 07:56:22 +0100

    Update embed.ts

--- src/embed.ts
+++ src/embed.ts
@@ -192,7 +192,7 @@ export class StarboardEmbed extends HTMLElement {
       const options = this.options;
       if (!options) return;
 
-      const checkOrigin = [new URL(options.src, location.origin ?? undefined).origin];
+      const checkOrigin = [new URL(options.src, location.origin ?? options.src).origin];
       if (!checkOrigin.includes(ev.origin)) return;
 
       if (!ev.data) return;
